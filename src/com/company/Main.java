2b6c99d1c59501034ba6b65b72a7a35c92e000f1
Raza <rtshareef@gmail.com>
2021-06-30 15:59:06 -0400

    gui is responsive to amount of times button is clicked

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,24 +2,35 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main {
+public class Main implements ActionListener {
+
+    int count = 0;
+    JLabel jlabel;
+    JFrame jframe;
+    JPanel panel;
 
     public Main(){
-        JFrame frame = new JFrame();
+         jframe = new JFrame();
         JButton jbutton = new JButton("Click");
+        jbutton.addActionListener(this);
+         jlabel = new JLabel("Number of Clicks");
 
-        JPanel panel = new JPanel();
+         panel = new JPanel();
 
         panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
         panel.setLayout(new GridLayout(0,1));
         panel.add(jbutton);
+        panel.add(jlabel);
 
-        frame.add(panel,BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Raza GUI");
-        frame.pack();
-        frame.setVisible(true);
+
+        jframe.add(panel,BorderLayout.CENTER);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.setTitle("Raza GUI");
+        jframe.pack();
+        jframe.setVisible(true);
 
 
     }
@@ -27,5 +38,12 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         new Main();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        jlabel.setText("number of clicks " + count);
+
     }
 }
