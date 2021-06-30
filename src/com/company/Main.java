d99371cf79bd8d47ef3fd5abc6077879394332bf
Raza <rtshareef@gmail.com>
2021-06-30 15:50:53 -0400

    added button to jpanel

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,13 @@ public class Main {
 
     public Main(){
         JFrame frame = new JFrame();
+        JButton jbutton = new JButton("Click");
+
         JPanel panel = new JPanel();
 
         panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
         panel.setLayout(new GridLayout(0,1));
+        panel.add(jbutton);
 
         frame.add(panel,BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
