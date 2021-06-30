e4e771bb1d0f2222e54f0510b52519b6d399cece
Raza <rtshareef@gmail.com>
2021-06-30 15:48:12 -0400

    initialized gui

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,28 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+
+    public Main(){
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+
+        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+        panel.setLayout(new GridLayout(0,1));
+
+        frame.add(panel,BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Raza GUI");
+        frame.pack();
+        frame.setVisible(true);
+
+
+    }
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hello");
+        new Main();
     }
 }
